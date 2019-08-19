9f5d147ca289ed4309e917a490741c16c125e94d
Brian Duncan Johnson <brian.duncan.johnson@sallinggroup.com>
2019-08-19 12:17:34 +0200

    Setup firebase integration

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,43 @@
-import React from "react";
+import { React, Component } from "react";
+import { Link } from 'react-router-dom';
 import Typography from "@material-ui/core/Typography";
+
+import * as ROUTES from "constants/routes.js"
 
 const SignUp = () => (
   <div>
     <Typography variant="h3" align="center" component="h1" gutterBottom>
       Sign Up
+      <SignUpForm />
     </Typography>
   </div>
 );
 
+class SignUpForm extends Component {
+  onSubmit = event => {
+
+  }
+
+  onChange = event => {
+
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+
+      </form>
+    )
+  }
+
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link  to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+)
+
 export default SignUp;
+
+export {SignUpForm, SignUpLink };
